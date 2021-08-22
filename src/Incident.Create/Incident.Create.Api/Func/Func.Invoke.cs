bfdfb4d500d0dd35c8034064383a04c51ae05daf
Дмитрий новиков <idmitriy1998@gmail.com>
2021-08-22 18:03:46 +0300

    Created skeleton of function

---
+++ src/Incident.Create/Incident.Create.Api/Func/Func.Invoke.cs
@@ -0,0 +1,21 @@
+﻿#nullable enable
+
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GGroupp.Internal.Support
+{
+    partial class IncidentCreateFunc
+    {
+        public partial ValueTask<Result<IncidentCreateOut, Failure<IncidentCreateFailureCode>>> InvokeAsync(
+            IncidentCreateIn input, CancellationToken cancellationToken)
+            =>
+            AsyncPipeline.Start(
+                input ?? throw new ArgumentNullException(nameof(input)),
+                cancellationToken)
+            .Pipe(
+                )
+    }
+}
---
+++ src/Incident.Create/Incident.Create.InOut/IncidentCreateIn.cs
@@ -0,0 +1,33 @@
+﻿#nullable enable
+
+using System;
+#nullable enable
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace GGroupp.Internal.Support
+{
+    public sealed record IncidentCreateIn
+    {
+        public Guid ActiveDirectoryUserId { get; }
+
+        public Guid CustomerId { get; }
+
+        public string Title { get; }
+
+        public string Description { get; }
+
+        public IncidentCreateIn(
+            Guid activeDirectoryUserId,
+            Guid customerId,
+            string title,
+            [AllowNull] string description)
+        {
+            ActiveDirectoryUserId = activeDirectoryUserId;
+            CustomerId = customerId;
+            Title = title ?? string.Empty;
+            Description = description ?? string.Empty;
+        }
+
+    }
+}
