8d725c4f21721f2712ba11b2dd69c6ba1ddb8492
Pavel Moskovoy <p@moskovoi.ru>
2021-11-15 12:16:40 +0300

    Update IncidentJsonCreateOut.cs

--- src/Incident.Create/Incident.Create.Api/Json/IncidentJsonCreateOut.cs
+++ src/Incident.Create/Incident.Create.Api/Json/IncidentJsonCreateOut.cs
@@ -6,7 +6,7 @@ namespace GGroupp.Internal.Support;
 internal readonly record struct IncidentJsonCreateOut
 {
     [JsonPropertyName(ApiNames.IncidentId)]
-    public Guid IncidentId{ get; init; }
+    public Guid IncidentId { get; init; }
 
     [JsonPropertyName(ApiNames.Title)]
     public string? Title { get; init; }
